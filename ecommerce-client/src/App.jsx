b40474b88bf2cd1756e6872c367e8514fa114e05
Sagar <dev.sagartmg@gmail.com>
2023-09-22 17:02:28 +0545

    products in home

--- ecommerce-client/src/App.jsx
+++ ecommerce-client/src/App.jsx
@@ -1,12 +1,35 @@
 import Banner from "./components/Banner"
 import Header from "./components/Header"
+import Chair from "./assets/chair.png"
+import { AiOutlineShoppingCart } from "react-icons/ai"
 
 function App() {
-
   return (
     <>
       <Header />
-      <Banner/>
+      <Banner />
+      <div className="container my-20">
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {
+            [1, 2, 3, 4].map(el => {
+              return <div className="relative shadow-xl pt-4 border border-transparent text-center hover:border hover:border-primary group">
+                <img src={Chair} alt="" className="mx-auto" />
+                <div className="group-hover:bg-primary p-4">
+                  <p className="text-secondary font-bold text-lg group-hover:text-white">Product Name</p>
+                  <p className="text-[#151875] text-sm group-hover:text-white">$100</p>
+                </div>
+                <span className="hidden group-hover:inline-block p-4 border border-2 absolute top-4 left-4 rounded-full bg-[#EEEFFB]"
+                  onClick={() =>{alert("add to cart")}}
+                >
+                  <AiOutlineShoppingCart className="text-primary-dark text-2xl" />
+                </span>
+              </div>
+            })
+          }
+        </div>
+
+
+      </div>
     </>
   )
 }
--- ecommerce-client/src/components/Banner.jsx
+++ ecommerce-client/src/components/Banner.jsx
@@ -8,15 +8,15 @@ export default function Banner() {
                 style={{
                     backgroundImage: `url(${BannerImage})`
                 }}>
-                <div className="container mt-20" >
+                <div className="container mt-8 lg:mt-20" >
                     <div className="w-1/2">
                         <p className="text-secondary font-bold mb-3">Best Furniture For Your Castle....</p>
-                        <p className="text-5xl font-bold mb-7">
+                        <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 lg:mb-7">
                             New Furniture CollectionTrends in 2020
                         </p>
-                        <p className="text-primary-light mb-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscingin phasellus non in justo.
+                        <p className="text-primary-light  mb-2 lg:mb-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscingin phasellus non in justo.
                         </p>
-                        <button className="bg-secondary text-white px-6 py-3">Shop Now</button>
+                        <button className="bg-secondary text-white px-3 py-1 lg:px-6 lg:py-3">Shop Now</button>
                     </div>
                 </div>
             </div>
@@ -28,7 +28,7 @@ export default function Banner() {
                 <h1>hello</h1>
             </div>
             {/* <img src={BannerImage} alt="banner" /> */}
-            <img src="./assets/banner-1.png" alt="banner" />
+            {/* <img src="./assets/banner-1.png" alt="banner" /> */}
             {/* <img src="https://fastly.picsum.photos/id/116/1920/1080.jpg?hmac=6YPBGD1f3FmqVLG2HweZHfprOzOVVBafm7EJ50g9iSU" alt="" /> */}
         </>
     )
